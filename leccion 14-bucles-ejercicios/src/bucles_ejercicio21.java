81e50ef8fa9448f5dde830e0c92d644abb773289
guz2001 <guz20dgc@gmail.com>
2026-02-18 10:27:43 -0500

    terminacion ejercicio21  video 40  bucles

--- leccion 14-bucles-ejercicios/src/bucles_ejercicio21.java
+++ leccion 14-bucles-ejercicios/src/bucles_ejercicio21.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class bucles_ejercicio21 {
+    public static void main(String[] args){
+    /*pediar 10 numeros
+    mostrar si se metio algun negativo al final
+     */
+        Scanner ne= new Scanner(System.in);
+        int numeros,conteoNumerosPos=0,conteoNumerosNeg=0;
+        boolean siNegativos=false;
+        for (int i=1;i<=10;i++){
+            System.out.println("ingrese 10  numeros: ");
+            numeros=ne.nextInt();
+
+            if (numeros>0){
+                conteoNumerosPos++;
+                siNegativos=false;
+            }else if(numeros<0) {
+                conteoNumerosNeg++;
+                siNegativos=true;
+
+            }else{
+                System.out.println("Numero erroneo ");
+            }
+
+        }
+        System.out.println("si hay negativos es igual a "+ siNegativos);
+        System.out.println("cantidad numeros negativos: "+conteoNumerosNeg );
+        System.out.println("numeros negtativos fueron de  " + conteoNumerosPos );
+
+    }
 }
