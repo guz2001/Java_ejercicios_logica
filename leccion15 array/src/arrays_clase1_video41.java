caff119c08499bc10cac6a0e57b1e49daa4184bf
guz2001 <guz20dgc@gmail.com>
2026-02-18 21:13:28 -0500

    terminacion ejercicio23 arreglos no poner= da error al leer longiutd

---
+++ leccion15 array/src/arrays_clase1_video41.java
@@ -0,0 +1,31 @@
+import java.sql.Array;
+import java.util.Arrays;
+
+public class arrays_clase1_video41 {
+    public static void main(String[] args){
+        //se puede definir uno por uno asi
+        int [] edad = new int[5];
+        edad[0]=1;
+        edad[1]=3;
+        edad[2]=5;
+        edad[3]=6;
+        edad[4]=7;
+        System.out.println(edad[0]);
+        // o todos asi
+
+        for(int i=0;i<edad.length;i++){//evitar poner igual a la longitud del array por que da error
+            System.out.println("iteraciones"+edad[i]);
+        }
+
+
+        String [] letras= new String[7];
+        for (int i=0;i<letras.length;i++){
+            System.out.println("valor nuevo es"+ (letras[i]="juandavid"+i));
+
+
+
+        }
+
+
+    }
+}
