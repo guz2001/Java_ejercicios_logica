e7150066092f5dff827db1bf03bcfed2e2472adf
guz2001 <guz20dgc@gmail.com>
2026-02-20 17:56:53 -0500

    terminacion  arreglo ejercicios 5

--- leccion15 array/src/array_ejercicio5_ab.java
+++ leccion15 array/src/array_ejercicio5_ab.java
@@ -1,8 +1,30 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class array_ejercicio5_ab {
     public static void main(String[] args){
         Scanner in= new Scanner(System.in);
+        int [] A= new int[10];
+        int [] B= new int[10];
+
+        //String D= Arrays.toString(B);
+        Integer [] ul= new Integer[20];
+
+        int arrayA=0,arrayB=0;
+        for(int i=0;i<10;i++){
+            System.out.print("INGRESE EL de A:-> "+ (i+1)+" ");
+            arrayA=A[i]=in.nextInt();
+            System.out.print("INGRESE EL de B : ->"+ (i+1)+" ");
+            arrayB=B[i]= in.nextInt();
+
+
+        }
+        for (int i=0;i<20;i++){
+            String C= Arrays.toString(A)+" "+Arrays.toString(B);
+            System.out.println(C);
+        }
+
+
 
     }
 }
