076182aa9c92909e7d20a18b2a1ef9a0a0ecccb6
guz2001 <guz20dgc@gmail.com>
2026-02-20 20:59:08 -0500

    terminacion  arreglo ejercicios 6 medio hecho

--- leccion15 array/src/array_ejercicio6.java
+++ leccion15 array/src/array_ejercicio6.java
@@ -10,9 +10,29 @@ public class array_ejercicio6 {
         int [] numB = new int[12];
         int [] numC=new int [24];
         for(int i=0;i<12;i++){
-            System.out.println("");
+            System.out.println(" a  ");
             numA [i]=scan.nextInt();
+            System.out.println(" b ");
+            numB[i]=scan.nextInt();
         }
+        int it=0;
+        for(int i=0;i<24;i++){
+            numC[it]=numA[i];
+            it++;
+            numC[it]=numB[i];
+            it++;
+            numC[it+1]=numA[i];
+            it++;
+            numC[it+1]=numB[i];
+            it++;
+            numC[it+2]=numA[i];
+            it++;
+            numC[it+2]=numA[i];
+            System.out.println(numC[it]);
 
+        }
+        for(int i=0;i<24;i++){
+            System.out.println("menor: "+numC[it]+" mayor ");
+        }
     }
 }
