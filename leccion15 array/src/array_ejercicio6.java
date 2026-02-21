e11dacc1893878a49b70f1c05e57f37d1fd27743
guz2001 <guz20dgc@gmail.com>
2026-02-20 20:42:20 -0500

    terminacion  arreglo ejercicios 6

--- leccion15 array/src/array_ejercicio6.java
+++ leccion15 array/src/array_ejercicio6.java
@@ -6,6 +6,13 @@ import java.util.Scanner;
 public class array_ejercicio6 {
     public static void main(String[] args){
         Scanner scan= new Scanner(System.in);
+        int [] numA=new int[12];
+        int [] numB = new int[12];
+        int [] numC=new int [24];
+        for(int i=0;i<12;i++){
+            System.out.println("");
+            numA [i]=scan.nextInt();
+        }
 
     }
 }
