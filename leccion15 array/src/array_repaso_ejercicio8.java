171da0229b527b843bca07f3c9abd145092ceb3d
guz2001 <guz20dgc@gmail.com>
2026-02-27 18:53:04 -0500

    arrays  agregar ultimos dos repaso

---
+++ leccion15 array/src/array_repaso_ejercicio8.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class array_repaso_ejercicio8 {
+    public static void main(String[] args){
+        Scanner es= new Scanner(System.in);
+        int [] tenArray= new int[10];
+        int j=0,addNumber,position;
+        for(int i=0; i<8;i++){
+            System.out.println("ingrese un numero");
+            tenArray[i]= es.nextInt();
+            j++;
+
+        }
+        System.out.println("es j"+j);
+        for(int i=j;i<10;i++){
+            System.out.println("ingrese un numero para agregar:");
+            tenArray[i]= es.nextInt();
+            addNumber=tenArray[i];
+        }
+        for(Integer arrayResultado: tenArray){
+            System.out.println("resultado del array es: "+arrayResultado);
+        }
+    }
+}
