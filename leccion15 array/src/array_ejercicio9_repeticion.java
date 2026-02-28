dde99044a1c1b40f1482e2eb2040365b92fca67a
guz2001 <guz20dgc@gmail.com>
2026-02-27 19:46:31 -0500

    ejercicio 9 acabamos ahora si entndi  muy bien

---
+++ leccion15 array/src/array_ejercicio9_repeticion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class array_ejercicio9_repeticion {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int []array= new int[10];//creamos el array con 10 espacio
+        for(int i=0;i<10;i++){// pedimos al usuario 10 datos
+            System.out.println("escoja numero");
+            array[i]=sc.nextInt();
+        }
+        int ultimo=array[9];//guardamos el valor del ultimo array
+        for(int i=8;i>=0;i--){//vamos disminuyendo
+            array[i+1]=array[i];// el array n copia al array n-1 osea array8 =10 toma el valor de array7=9 osea quedaria
+            //array8 =9 array 7=9 duplicado hasta el mas bajo
+        }
+        array[0]=ultimo;//el ultimo arra que queda con valor 1 toma el valor del ultimo que era 10 o el ultimo digitado
+        for(Integer lista: array){//hacemos un for each para imprimir los valores de cada uno evidenciando que se logro el objetivo
+            System.out.println("valor array es: "+ lista);
+        }
+
+    }
+}
