3e9e820e8cd0d0f38140711356ea6ef42b01c62c
guz2001 <guz20dgc@gmail.com>
2026-02-27 19:39:53 -0500

    ejercicio 9 acabamos pero no entendi muy bien

--- leccion15 array/src/array_ejercicio9.java
+++ leccion15 array/src/array_ejercicio9.java
@@ -3,6 +3,23 @@ import java.util.Scanner;
 public class array_ejercicio9 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+        int [] tablaDiez= new int[10];
+        int inverso=0,a,b;
+        for(int i=0;i<10;i++){
+            System.out.printf("ingrese numero para espacio %d\n",i);
+            tablaDiez[i]=sc.nextInt();
+
+        }
+        inverso=tablaDiez[9];
+        for(int i=8;i>=0;i--){
+
+            tablaDiez[i+1]=tablaDiez[i];//  el de la posicion 1 sera igual al de la posicion
+            System.out.println("arreglo "+tablaDiez[i]+"posicion es: "+i);
+        }
+        tablaDiez[0]=inverso;
+        for(int i=0; i<10;i++){
+            System.out.println("es "+ tablaDiez[i]);
+        }
 
     }
 }
